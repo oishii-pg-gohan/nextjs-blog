0ec7109923433f3ca4cbd586627d32471f5cac96
oishii-pg-gohan <oishii.pg.gohan@gmail.com>
2020-06-01 17:11:51 +0900

    convert typescript from js

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,10 +3,10 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = "dev1 branch's modify."
+const name = "Could I modify by typescript??"
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home }: { children: React.ReactNode, home?: boolean }) {
     return (
         <div className={styles.container}>
             <Head>
