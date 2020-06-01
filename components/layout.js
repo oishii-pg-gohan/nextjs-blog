630c1352a9da3211dd993b3dde925f1d6a316112
oishii-pg-gohan <oishii.pg.gohan@gmail.com>
2020-06-01 16:02:20 +0900

    Change index title.

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'My first Next.js project'
+const name = 'Modified index title (and push to github).'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
